1bc9a48f981e8312c688bb2302859d224e71a175
mcitla <mcitla@gmail.com>
2015-03-21 19:21:32 -0600

    I did it?

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To clone the assignment i put the following line in git bash 
+## git clone https://github.com/aresendiz/ProgrammingAssignment2.git
+## makeCacheMatrix will set a matrix, then get the value of the matrix
+## set a value of the inverse of the matrix Input (Inp) and get the value of this inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    Inverse<- NULL
+    set <- function(Inp) {
+        x <<- Inp
+        Inverse<<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) Inverse<<- solve 
+    getsolve <- function() Inverse
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function will return the inverse of a matrix
+## using the solve stated function
+## It starts checking if it has been computed already
+## If it has not, it will compute the numbers 
+## it it has then it gets to the cache and returns its value
+## you can check if this value in in the cache since it returns
+## the message: "Using cache data"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    Inverse<- x$getsolve()
+    if(!is.null(Inverse)) {
+        message("Using cached data")
+        return(Inverse)
+    }
+    data <- x$get()
+    Inverse<- solve(data, ...)
+    x$setsolve(Inverse)
+    Inverse
 }
+
+## to check if this program works try the following files 
+## in the console after running everything! 
+## x<- matrix(1:6,3,2)
+## Inverse<- makeCacheMAtrix(x)
+## cacheSolve(Inverse)
+## cacheSolve(Inverse)
